01c5ed1a8377c1b917ec1bf7a3de55ae24040538
Will Elson <elson594@gmail.com>
2025-01-20 18:37:56 +0100

    useTheme hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
+import { ThemeProvider } from "@/components/theme-provider";
 import { Button } from "@/components/ui/button";
 import ThemeToggle from "@/ThemeSwitcher";
 
 function App() {
   return (
-    <div className="bg-background">
-      <h1 className="text-foreground text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <Button>Click me</Button>
-      <ThemeToggle />
-    </div>
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <div className="bg-background">
+        <h1 className="text-foreground text-3xl font-bold underline">
+          Hello world!
+        </h1>
+        <Button>Click me</Button>
+        <ThemeToggle />
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/ThemeSwitcher.tsx
+++ src/ThemeSwitcher.tsx
@@ -1,48 +1,23 @@
+import { useTheme } from "@/components/theme-provider";
 import { Button } from "@/components/ui/button";
-import { useEffect, useState } from "react";
-
-type Theme = "light" | "dark";
+import { Moon, Sun } from "lucide-react";
 
 const ThemeToggle = () => {
-  const [theme, setTheme] = useState<Theme>("dark");
-
-  useEffect(() => {
-    const systemPrefersDark =
-      window.matchMedia &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches;
-
-    const localStorageTheme = localStorage.getItem("theme") as
-      | Theme
-      | undefined;
-
-    if (localStorageTheme) {
-      setTheme(localStorageTheme);
-    } else if (systemPrefersDark) {
-      setTheme("dark");
-    }
-  });
-
-  useEffect(() => {
-    const htmlElement = document.documentElement;
-    if (theme === "dark") {
-      htmlElement.classList.add("dark");
-    } else {
-      htmlElement.classList.remove("dark");
-    }
-  }, [theme]);
+  const { theme, setTheme } = useTheme();
 
   const toggleTheme = () => {
     if (theme === "dark") {
       setTheme("light");
-      localStorage.setItem("theme", "light");
     } else {
       setTheme("dark");
-      localStorage.setItem("theme", "dark");
     }
   };
 
-  const buttonText = theme === "dark" ? "light mode" : "dark mode";
-  return <Button onClick={toggleTheme}>{buttonText}</Button>;
+  return (
+    <Button onClick={toggleTheme}>
+      {theme === "dark" ? <Sun /> : <Moon />}
+    </Button>
+  );
 };
 
 export default ThemeToggle;
