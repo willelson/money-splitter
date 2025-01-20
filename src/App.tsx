74f7703d999843851d92c7035a0ec8e88093298a
Will Elson <elson594@gmail.com>
2025-01-20 18:01:11 +0100

    Button to switch color scheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { Button } from "@/components/ui/button";
+import ThemeToggle from "@/ThemeSwitcher";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="bg-background">
+      <h1 className="text-foreground text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Button>Click me</Button>
+      <ThemeToggle />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/ThemeSwitcher.tsx
@@ -0,0 +1,28 @@
+import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
+
+const ThemeToggle = () => {
+  const [theme, setTheme] = useState<"light" | "dark">("dark");
+
+  useEffect(() => {
+    const htmlElement = document.documentElement;
+    if (theme === "dark") {
+      htmlElement.classList.add("dark");
+    } else {
+      htmlElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  const toggleTheme = () => {
+    if (theme === "dark") {
+      setTheme("light");
+    } else {
+      setTheme("dark");
+    }
+  };
+
+  const buttonText = theme === "dark" ? "light mode" : "dark mode";
+  return <Button onClick={toggleTheme}>{buttonText}</Button>;
+};
+
+export default ThemeToggle;
