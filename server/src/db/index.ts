54c092f69111e0a9fb89b36472f0849fcc8fbdac
Will Elson <elson594@gmail.com>
2025-02-22 18:28:37 +0100

    Fix imports

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -5,10 +5,6 @@ import * as groupsSchema from "./schema/groups.js";
 import * as transactionRecipientsSchema from "./schema/transactionRecipients.js";
 import * as transactionsSchema from "./schema/transactions.js";
 import * as usersSchema from "./schema/users.js";
-import * as groupsSchema from "./schema/groups";
-import * as transactionRecipientsSchema from "./schema/transactionRecipients";
-import * as transactionsSchema from "./schema/transactions";
-import * as usersSchema from "./schema/users";
 
 export const db = drizzle(process.env.DATABASE_URL!, {
   schema: {
