78eddf5973bc1f42bfd75dc554165a73743b1ed4
Will Elson <elson594@gmail.com>
2025-02-27 22:15:56 +0100

    Update docker files

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -1,12 +1,14 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
+const DATABASE_URL = `postgresql://postgres:${process.env.POSTGRES_PASSWORD}@db:5432/postgres`;
+
 export default defineConfig({
   out: "./src/db/migrations",
   schema: "./src/db/schema",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL as string,
+    url: DATABASE_URL,
   },
   verbose: true,
   strict: true,
