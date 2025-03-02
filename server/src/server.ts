22267b68a17a1745152b6d7ea8d9cc56f44a6340
Will Elson <elson594@gmail.com>
2025-03-02 16:13:03 +0100

    Update CORS and add log

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,11 +6,13 @@ import { appRouter } from "./routers/index";
 
 const app = express();
 
+// app.use(cors({ origin: process.env.FRONTEND_URL }));
+app.use(cors());
 app.options("*", cors());
-app.use(cors({ origin: process.env.FRONTEND_URL }));
 app.use("/api", createExpressMiddleware({ router: appRouter }));
 
 const PORT = process.env.PORT || 3000;
+console.log(`PORT = ${PORT}`);
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 export type AppRouter = typeof appRouter;
