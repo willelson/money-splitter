7d8c56f95ea942a34e7258c8c568c2c4567be6c8
Will Elson <elson594@gmail.com>
2025-03-02 17:23:03 +0100

    Remove comments etc

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,24 +6,15 @@ import { appRouter } from "./routers/index";
 
 const app = express();
 
-// app.use(cors({ origin: process.env.FRONTEND_URL }));
 app.use(cors());
 app.options("*", cors());
 app.use("/api", createExpressMiddleware({ router: appRouter }));
-
-// const PORT = parseInt(process.env.PORT) || 3000;
 
 let port = 3000;
 
 if (process.env.PORT) {
   port = parseInt(process.env.PORT);
 }
-
-console.log(`port = ${port}`);
-
-app.get("/test", (_, res) => {
-  res.send(`port = ${port}`);
-});
 
 app.listen(port, "0.0.0.0", () =>
   console.log(`Server running on port ${port}`)
