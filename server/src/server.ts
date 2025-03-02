919a1fe113fabc052167e4a9b2bd94a96964ea35
Will Elson <elson594@gmail.com>
2025-03-02 16:34:18 +0100

    Update server hostname

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,13 +11,22 @@ app.use(cors());
 app.options("*", cors());
 app.use("/api", createExpressMiddleware({ router: appRouter }));
 
-const PORT = process.env.PORT || 3000;
-console.log(`PORT = ${PORT}`);
+// const PORT = parseInt(process.env.PORT) || 3000;
+
+let port = 3000;
+
+if (process.env.PORT) {
+  port = parseInt(process.env.PORT);
+}
+
+console.log(`port = ${port}`);
 
 app.get("/test", (_, res) => {
-  res.send(`PORT = ${PORT}`);
+  res.send(`port = ${port}`);
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(port, "0.0.0.0", () =>
+  console.log(`Server running on port ${port}`)
+);
 
 export type AppRouter = typeof appRouter;
